7b206b65315137ff4a3f4fcf5d76cdacdd6c1c55
Coding Scenes <codingscenes@gmail.com>
2023-02-20 01:29:14 +0530

    ..

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-// console.log(routes.myName);
+const app = express();
 
-const server = http.createServer(routes);
+// adding middleware
+app.use('/', (req, res, next) => {
+  console.log('Middleware always running!');
+  // sending response from here
+  res.send('<h2>hello</h2>');
+});
 
-server.listen(3000);
+app.use('/add-users', (req, res, next) => {
+  console.log('In the middlware-add-users');
+  // sending response from here
+  res.send('<h1>These are the users!</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In the middlware-root');
+  // sending response from here
+  res.send('<h1>Hello from Express JS!</h1>');
+});
+
+app.listen(3000);
