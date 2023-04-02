f21595ad6bdddcdc5b1f45493d2b579e8ad6247a
Coding Scenes <codingscenes@gmail.com>
2023-04-02 16:03:09 +0530

    .

--- db/sql/sequelize.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize").Sequelize;
-
-const sequelize = new Sequelize("nodejs-course", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-  logging: false, //to hide the verbose
-});
-
-module.exports = sequelize;
