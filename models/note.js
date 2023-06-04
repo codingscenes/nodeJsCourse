cae33f35a2e96e940b6d230fef0f8c596df3be24
Coding Scenes <codingscenes@gmail.com>
2023-06-04 16:52:02 +0530

    Update note.js

--- models/note.js
+++ models/note.js
@@ -1,22 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
 const getDb = require('../connection/db').getDb;
-
-const pathToFile = path.join(
-  path.dirname(require.main.filename),
-  'data',
-  'notes.json'
-);
-
-const getDataFromFile = (callbackFn) => {
-  fs.readFile(pathToFile, (err, fileContent) => {
-    if (err) {
-      return callbackFn([]);
-    }
-    callbackFn(JSON.parse(fileContent));
-  });
-};
 
 module.exports = class Note {
   constructor(_noteId, _title, _description, _imageUrl) {
@@ -33,16 +15,7 @@ module.exports = class Note {
   }
 
   saveChanges() {
-    getDataFromFile((notes) => {
-      const noteIndex = notes.findIndex((n) => n.noteId === this.noteId);
-      const notesCopy = [...notes];
-      notesCopy[noteIndex] = this;
-      fs.writeFile(pathToFile, JSON.stringify(notesCopy), (err) => {
-        if (err) {
-          console.log('error in saving file', err);
-        }
-      });
-    });
+
   }
 
   static fetchAll(callbackFn, isAdmin) {
@@ -51,38 +24,14 @@ module.exports = class Note {
   }
 
   static findNoteById(noteId, callbackFn) {
-    getDataFromFile((notes) => {
-      const note = notes.find((_note) => _note.noteId === noteId);
-      callbackFn(note);
-    });
+
   }
 
   static delete(noteId) {
-    getDataFromFile((_notes) => {
-      const notes = _notes.filter((n) => n.noteId !== noteId);
-      fs.writeFile(pathToFile, JSON.stringify(notes), (err) => {
-        if (err) {
-          console.log('error in saving file', err);
-        }
-      });
-    });
+
   }
 
   static approve(noteId) {
-    getDataFromFile((notes) => {
-      const noteIndex = notes.findIndex((n) => n.noteId === noteId);
-      const notesCopy = [...notes];
-      const singleNote = notesCopy[noteIndex];
-      const notesToApprove = {
-        ...singleNote,
-        status: singleNote.status === 'approved' ? 'unapproved' : 'approved',
-      };
-      notesCopy[noteIndex] = notesToApprove;
-      fs.writeFile(pathToFile, JSON.stringify(notesCopy), (err) => {
-        if (err) {
-          console.log('error in saving file', err);
-        }
-      });
-    });
+
   }
 };
